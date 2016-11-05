75854939d50fca94bd389e30c9f204cf333a241e
M.A.N <maxnaza@protonmail.ch>
2016-11-04 21:35:04 -0400

    defined menu and how text editor window shall be lunched

---
+++ src/com/company/TextEditor.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import javax.swing.*;
+
+public class TextEditor {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        new TextEditor();
+
+        class TextEditor {
+
+            // defining Menus
+            private JMenu file;
+            private JMenu edit;
+
+            // defining Window
+            private JFrame editorWindow; // this window actually text editor window
+        }
+    }
+}
