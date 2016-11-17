0633036e1be7a5520bdf9ee11e186d335fe6ff5a
M.A.N <maxnaza@protonmail.ch>
2016-11-16 21:52:22 -0500

    defined menu items and text area

--- src/com/company/TextEditor.java
+++ src/com/company/TextEditor.java
@@ -1,29 +1,64 @@
 package com.company;
 //package texteditor;
 
-import java.awt.event.ActionEvent;
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
-    
 
 public class TextEditor extends JFrame implements ActionListener {
-            
+
     public static void main(String[] args) {
-	 new TextEditor();
+        new TextEditor();
     }
 
-            // menu
-            private JMenu file;
-            private JMenu edit;
+    private JMenu file; //define file menu
 
-            // defining menu items
-            private JMenuItem new_file, open_file, save_file, saveas_file, delete_file;
-            private JMenuItem selectAll, cut, copy, paste;
+    private JMenu edit; //define edit menu
 
-            private JFrame editorWindow;
+    private JMenuItem newFile; //define new file
 
-            private JFrame window;
+    private JMenuItem open; //define open file
+
+    private JMenuItem save; //define save file
+
+    private JMenuItem saveAs; //define save file as
+
+    private JMenuItem delete; //define delete file
+
+    private JMenuItem selectAll; //define select all text
+
+    private JMenuItem cut; //define cut text
+
+    private JMenuItem copy; //define copy text
+
+    private JMenuItem paste; //define paste text
+
+    private JFrame editorWindow; //define text editor window
+
+    private Border textBorder; //define text border
+
+    private JScrollPane scroll; //define scroll
+
+    private JTextArea textArea; //define text area
+
+    private Font textFont; //define text font
+
+    private JFrame window; //define window to open or save file
+
+    private boolean opened = false; //is file opened
+
+    private boolean saved = false; //is file saved
+
+    private File openedFile;
+
+    public JTextArea getTextArea() {
+        return textArea;
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
