83193e24361a7af60190ffcd4c1171872394429d
M.A.N <maximillianaza@gmail.com>
2017-07-19 21:39:42 -0400

    Revert "Revert "fixed class name & method name to follow best programming practices""
    This reverts commit 00fc2a3c13f9680a01f21f91b73444c6b6658b1b.

--- src/com/company/Customize.java
+++ src/com/company/Customize.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 /**
  * Created by leemartinc on 11/30/16.
  */
-public class customize {
+public class Customize {
     private JComboBox comboBox1;
     private JComboBox comboBox2;
 }
