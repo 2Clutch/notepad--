af010a4ee59651070ceef7ef7ae5e15f9930aac0
M.A.N <maximillianaza@gmail.com>
2017-07-19 22:04:26 -0400

    declare fields at the top of the class to follow best programming practices

--- src/com/company/TextEditor.java
+++ src/com/company/TextEditor.java
@@ -11,28 +11,15 @@ import java.io.*;
 
 public class TextEditor extends JFrame implements ActionListener {
 
-    public static void main(String[] args) {
-        new TextEditor();
-    }
-
     private JMenu file; //define file menu
-
     private JMenu edit; //define edit menu
-
     private JMenuItem newFile; //define new file
-
     private JMenuItem open; //define open file
-
     private JMenuItem save; //define save file
-
     private JMenuItem saveAs; //define save file as
-
     private JMenuItem delete; //define delete file
-
     private JMenuItem selectAll; //define select all text
-
     private JMenuItem customize; //define Customize text
-
     private JFrame cust;
 
     private JComboBox jcomp1;
@@ -45,28 +32,21 @@ public class TextEditor extends JFrame implements ActionListener {
     private JFrame frame;
 
     private JMenuItem cut; //define cut text
-
     private JMenuItem copy; //define copy text
-
     private JMenuItem paste; //define paste text
-
     private JFrame editorWindow; //define text editor window
-
     private Border textBorder; //define text border
-
     private JScrollPane scroll; //define scroll
-
     private JTextArea textArea; //define text area
-
     private Font textFont; //define text font
-
     private JFrame window; //define window to open or save file
-
     private boolean opened = false; //is file opened
-
     private boolean saved = false; //is file saved
-
     private File openedFile;
+
+    public static void main(String[] args) {
+        new TextEditor();
+    }
 
     public JTextArea getTextArea() {
         return textArea;
